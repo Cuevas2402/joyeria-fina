d420032c9940f3f48ae3c9c5ef8d2080d9c99eea
Alan Garcia Rodriguez <alan.garciar@udem.edu>
2023-10-14 13:22:02 -0600

    Obtener vehículos y mandar nombre de sucursal agregados

--- app/orders.py
+++ app/orders.py
@@ -1,5 +1,5 @@
 from app import app , mysql
-from flask import render_template, request, session, url_for, redirect
+from flask import render_template, request, session, url_for, redirect, jsonify
 import requests, math
 
 @app.route('/get-branches', methods = ['GET', 'POST'])
@@ -14,7 +14,7 @@ def get_branches():
         cursor.close()
 
         cursor = mysql.connection.cursor()
-        sql = "SELECT id, latitude, longitude FROM branch;"
+        sql = "SELECT id, name, latitude, longitude FROM branch;"
         cursor.execute(sql)
         branches_coordinates = cursor.fetchall()
         cursor.close()
@@ -25,11 +25,24 @@ def get_branches():
         for branch in branches_coordinates:
             branch_id, x1, y1 = branch
             distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-            distances.append((distance, branch_id))
+            distances.append((distance, branch_id, branch_name))
         
         distances.sort()
         shortest_distances = distances[:5]
 
-        nearest_branches = [distance[1] for distance in shortest_distances]
+        nearest_branches = [{'branch_id' : distance[1], 'branch_name' : distance[2]} for distance in shortest_distances]
 
-        return {'nearest_branches': nearest_branches}
+        return {'nearest_branches' : nearest_branches}
+
+@app.route('/get-vehicles', methods = ['GET', 'POST'])
+def get_vehicles():
+    if request.method == 'POST':
+        company_id = request.form.get('id')
+
+        cursor = mysql.connection.cursor()
+        sql = "SELECT id, name, fixed_price, km_fee FROM truck WHERE company_id = %s;"
+        cursor.execute(sql, (company_id, ))
+        vehicles = cursor.fetchall()
+        cursor.close()
+
+        return jsonify(vehicles)
